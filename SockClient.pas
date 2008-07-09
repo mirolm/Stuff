c99b678cadc3029ef7c7c35284b9c56b4731c6f8
ieti <ieti@34d96223-17fc-41c4-8fee-8eb129dce427>
2008-07-09 12:25:20 +0000

    - formatted

--- SockClient.pas
+++ SockClient.pas
@@ -1018,18 +1018,12 @@ end;
 function LoadFunc(LibHandle: THandle; var FuncPtr: FARPROC; const FuncName: string): Boolean;
 begin
   FuncPtr := nil;
+  Result := False;
 
   if (LibHandle <> 0) then
   begin
     FuncPtr := GetProcAddress(LibHandle, PChar(FuncName));
-    if Assigned(FuncPtr) then
-      Result := True
-    else
-      Result := False;
-  end
-  else
-  begin
-    Result := False;
+    Result := Assigned(FuncPtr);
   end;
 end;
 
